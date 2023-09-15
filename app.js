299c1b63ec087369e82765a826591e742d46acc7
ABDALRHMAN <abdalrhmanahmed00004@gmail.com>
2023-09-15 20:38:52 +0300

    make the code more clean

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let qBody = document.querySelector(".quiz-body");
 let Question = document.querySelector(".quiz-body h2");
 let footer = document.querySelector("quiz-footer");
-let nextButton = document.querySelector(".btn");
+let next = document.querySelector(".btn");
 let timer = document.querySelector(".timer");
 let secTimer = document.querySelector(".timer_sec");
 
@@ -9,94 +9,85 @@ let current = 0;
 let rAns = 0;
 let countInterval;
 
+// the first function to get the questions from json file and add it to the body of the page
 function getQuestions() {
+  // crate a request
   let myRequest = new XMLHttpRequest();
   myRequest.open("GET", "quistions.json", true);
   myRequest.send();
-  myRequest.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      // console.log(this.responseText);
-      let qObject = JSON.parse(this.responseText);
-      // console.log(qObject);
+  // check if the request is done and the status is 200
+  myRequest.onreadystatechange = () => {
+    if (myRequest.readyState === 4 && myRequest.status === 200) {
+      let qObject = JSON.parse(myRequest.responseText);
       let qCount = qObject.length;
-      // console.log(qCount);
       count(10, qCount);
-      
-      // the addQuestion function
+      // the second function is addQuestion function to add the question to the body
       addQuestion(qObject[current], qCount);
-      nextButton.onclick = () => {
-        // get the right answer
+      // event that when I click on the next button the current will increment by one
+      next.addEventListener("click", () => {
         let rightAnswers = qObject[current].right_answer;
-        // console.log(rightAnswer); done
         current++;
+        // the third function is rightAnswer function to check if the answer is right or not
         rightAnswer(rightAnswers, qCount);
         // remove the old question
         qBody.innerHTML = "";
         Question.innerHTML = "";
+        // add the new question
         addQuestion(qObject[current], qCount);
-        // show result
+        // the forth function is showResult function to show the result at the end of the quiz
         showResult(qCount);
-        // start timer
+        // the fifth function is the counter function to start the timer
         clearInterval(countInterval);
         count(10, qCount);
-      };
+      });
     }
   };
 }
-
+// call the first function
 getQuestions();
 
+// addQuestion function
 function addQuestion(qObject, qCount) {
+  // this statement to check if the current is less than the number of questions to not make an error when the questions finish
   if (current < qCount) {
-    // console.log(qCount);
-    // console.log(qObject);
+    // create the question and add it to body
     let qText = document.createTextNode(qObject["title"]);
-    // console.log(qText); done
     Question.className = "question";
     Question.appendChild(qText);
     qBody.appendChild(Question);
-    // done to add question to body
-    // create anwers and add to body
+    // create the answers and add it to body
     for (let i = 1; i <= 4; i++) {
       let answersDiv = document.createElement("div");
       answersDiv.className = "answers";
-      // create buttons
-      let answerI = document.createElement("input");
-      answerI.id = `answer_${i}`;
-      answerI.type = "radio";
-      answerI.name = "answer";
-      answerI.dataset.answer = qObject[`answer_${i}`];
-      // console.log(answer); done
-      // create labels
+      let answerInput = document.createElement("input");
+      answerInput.id = `answer_${i}`;
+      answerInput.type = "radio";
+      answerInput.name = "answer";
+      answerInput.dataset.answer = qObject[`answer_${i}`];
       let answer = document.createElement("label");
       answer.htmlFor = `answer_${i}`;
       answer.id = "answer";
       let answerText = document.createTextNode(qObject[`answer_${i}`]);
       answer.appendChild(answerText);
-      // console.log(answer); done
-      // append to div
-      answersDiv.appendChild(answerI);
+      answersDiv.appendChild(answerInput);
       answersDiv.appendChild(answer);
-      // append to body
       qBody.appendChild(answersDiv);
     }
   }
 }
 
 // rightAnswer function
-
 function rightAnswer(correctAnswer, qCount) {
   let answers = document.getElementsByName("answer");
   let chosenAnswer;
   for (let i = 0; i < answers.length; i++) {
     if (answers[i].checked) {
-      chosenAnswer = answers[i].dataset.answer; // Retrieve the data-answer attribute
+      chosenAnswer = answers[i].dataset.answer;
     }
   }
   if (chosenAnswer === correctAnswer) {
-    rAns++; // Increment rAns only if the chosen answer is correct
+    rAns++;
   }
-  // Check if this was the last question and show the result
   if (current === qCount - 1) {
     showResult(qCount);
   }
@@ -104,11 +95,12 @@ function rightAnswer(correctAnswer, qCount) {
 
 function showResult(qCount) {
   let result;
+  // check if the current is equal to the number of questions to show the result at the end of the quiz
   if (current === qCount) {
     qBody.innerHTML = "";
     Question.innerHTML = "";
     timer.innerHTML = "";
-    nextButton.style.display = "none";
+    next.style.display = "none";
     if (rAns === qCount) {
       result = document.createTextNode(
         `You are a genius, you got ${rAns} out of ${qCount}`
@@ -127,8 +119,9 @@ function showResult(qCount) {
   }
 }
 
-// ten second timer
+// count function
 function count(duration, count) {
+  // this statement to check if the current is less than the number of questions to not make an error when the questions finish
   if (current < count) {
     let sec = duration;
     countInterval = setInterval(() => {
@@ -136,7 +129,7 @@ function count(duration, count) {
       secTimer.innerHTML = sec;
       if (sec === 0) {
         clearInterval(countInterval);
-        nextButton.click();
+        next.click();
       }
     }, 1000);
   }
